f3161982e3bedf42849d678428006d445c78b175
ks24pon <pom123re0624@gmail.com>
2026-01-14 14:49:35 +0900

    演算子の優先順位

---
+++ very_easy/sample3.java
@@ -0,0 +1,8 @@
+class MyClass{
+    public static void main(String[] args){
+        System.out.println(2-81/3/3*3*3+8);
+        System.out.println(3+10/2*5);
+        System.out.println(4+39/(8+5)*2);
+        System.out.println(5+(80+1)/9/3*5);
+    }
+}
