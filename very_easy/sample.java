113ec50ef1eb98a88482716b870834aef4e93dab
ks24pon <pom123re0624@gmail.com>
2026-01-14 09:23:58 +0900

    ブーリアン型と整数型

---
+++ very_easy/sample.java
@@ -0,0 +1,9 @@
+class MyClass{
+    public static void main(String[] args){
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(9999);
+        System.out.println(0b1111);
+        System.out.println(0b11);
+    }
+}
