bfa1469c00e3dcecf6b75573e18733db31fdb207
ks24pon <pom123re0624@gmail.com>
2026-01-14 16:41:29 +0900

    インデックス演算子

---
+++ very_easy/sample5.java
@@ -0,0 +1,8 @@
+class MyClass{
+    public static void main(String[] args){
+        System.out.println("Recursion".charAt(0));
+        System.out.println("Recursion".charAt(4));
+        System.out.println("Recursion".charAt(8));
+
+    }
+}
