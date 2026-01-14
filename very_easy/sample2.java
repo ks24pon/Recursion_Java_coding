33be98de1f04068fa69b95c9642220eb7463da60
ks24pon <pom123re0624@gmail.com>
2026-01-14 13:24:58 +0900

    リテラル

---
+++ very_easy/sample2.java
@@ -0,0 +1,9 @@
+class MyClass{
+    public static void main(String[] args){
+        System.out.println("P");
+        System.out.println(2022);
+        System.out.println("I am learning primitive data. #RecursionCS");
+        System.out.println(12.3);
+        System.out.println(false);
+    }
+}
