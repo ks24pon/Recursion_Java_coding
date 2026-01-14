0b928c71f31a769701fb332431071d8c6707a0fa
ks24pon <pom123re0624@gmail.com>
2026-01-14 10:58:43 +0900

    文字列

---
+++ very_easy/sample1.java
@@ -0,0 +1,8 @@
+class MyClass{
+    public static void main(String[] args){
+        System.out.println("The earth was bluish.");
+        System.out.println("");
+        System.out.println(1 + 1);
+        System.out.println("1" + "1");
+    }
+}
