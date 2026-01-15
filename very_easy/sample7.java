5b12b168108f31421685809b378867d264c9c83a
ks24pon <pom123re0624@gmail.com>
2026-01-15 10:06:09 +0900

    変数の宣言と代入

---
+++ very_easy/sample7.java
@@ -0,0 +1,9 @@
+class MyClass{
+    public static void main(String[] args){
+        String myFavoriteFruit = "Apple";
+        System.out.println(myFavoriteFruit);
+
+        int appleCount = 5;
+        System.out.println(appleCount);
+    }
+}
