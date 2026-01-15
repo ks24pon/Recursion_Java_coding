e41460e6a49a2bcaf55d0cb133773e82a268d4d0
ks24pon <pom123re0624@gmail.com>
2026-01-15 10:15:46 +0900

    変数の上書き

---
+++ very_easy/sample8.java
@@ -0,0 +1,9 @@
+class MyClass{
+    public static void main(String[] args){
+        boolean isEmailValid = true;
+        System.out.println(isEmailValid);
+
+        boolean isEmaliValid = false;
+        System.out.println(isEmaliValid);
+    }
+}
