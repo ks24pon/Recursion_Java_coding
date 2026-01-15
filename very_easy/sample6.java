bbd0d9bbac474287f3522753526ece9962820e23
ks24pon <pom123re0624@gmail.com>
2026-01-15 09:01:56 +0900

    lengthプロパティ

---
+++ very_easy/sample6.java
@@ -0,0 +1,8 @@
+class MyClass{
+    public static void main(String[] args){
+        System.out.println("Computer".length());
+        System.out.println("recursion@info.com".length());
+        System.out.println("Computer".charAt(7));
+        System.out.println("recursion@info.com".charAt(17));
+    }
+}
